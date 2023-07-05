cf2c8e6d81b44cd9294462f1e1490440740f2448
Ricardo Fernández <deus_ith@hotmail.com>
2023-07-05 10:32:23 -0600

    Reads files to interpret avaliable endpoints

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,44 @@
+import { existsSync, readdirSync } from 'fs';
+
 import Link from 'next/link'
 
-export default function Homepage() {
+import { ENDPOINT_DIRECTORY } from '@/utils/constants'
+
+export const getEndpoints = async () => {
+  let response = {}
+  if (!existsSync(ENDPOINT_DIRECTORY)) {
+    response.noEndpoints = true;
+  } else {
+    response.endpointList = readdirSync(ENDPOINT_DIRECTORY);
+  }
+
+  return response;
+};
+
+const Homepage = async () => {
+  const { endpointList, noEndpoints } = await getEndpoints();
   return (
     <>
-      <>
-        <Link href="/new">Nuevo</Link>
-      </>
+      <p><Link href="/new">Nuevo</Link></p>
       <h2>Lista de endpoints disponibles</h2>
+      {
+        noEndpoints
+          ? (
+            <>
+              <p>No endpoints</p>
+              <p><Link href="/new">Create new?</Link></p>
+            </>
+          )
+          : (
+            <ul>
+              {
+                endpointList.map((ep) => <li>{ep}</li>)
+              }
+            </ul>
+          )
+      }
     </>
   )
 }
+
+export default Homepage;
