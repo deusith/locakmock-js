64b8d02756269341833f9a6e7f1e6ffc6ba32537
Ricardo Fernández <deus_ith@hotmail.com>
2023-07-07 08:59:51 -0600

    Finished basic form functionality

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -9,22 +9,71 @@ import { STATUS_LIST } from '@/utils/constants';
 const NewPage = () => {
 
   // User Entered data -- todo rename this shit.
-  const [endpointPath, setEndpointPath] = useState('mmipath');
+  const [endpointPath, setEndpointPath] = useState('');
   const [formFields, setFormFields] = useState([{
-    status: 200,
-    data: {
-      dasName: 'Ricardooooooo',
-      dasLastName: 'Fernandez'
-    },
+    status: undefined,
+    data: undefined,
   }]);
-
-  const addNewStatus = () => { };
-
-  const onChange = () => { };
-
-  const changePathHandler = (event) => {
-    setPath(event.target.value);
+  const [isJsonValid, setIsJsonValid] = useState(true);
+  const oneGroupForEachStatus = formFields.length === STATUS_LIST.length;
+  
+  // Temporal styles object
+  const textAreaStyle = {
+    outline: isJsonValid ? 'unset' : '2px solid red'
   }
+
+  // Creates a new status group for another status
+  const addNewStatusGroup = (event) => {
+    event.preventDefault();
+    setFormFields((prevState) => {
+      const newState = oneGroupForEachStatus
+        ? prevState
+        : [...prevState, {
+          status: undefined,
+          data: undefined,
+        }];
+      return newState;
+    });
+  };
+
+  // Removes a status group from the form.
+  const removeStatusGroup = (event) => {
+    event.preventDefault();
+    const row = Number(event.target.dataset.row);
+    setFormFields(
+      (prevState) => prevState.filter((e, index) => index !== row)
+    );
+  };
+
+  // Changes the path name the endpoint will have.
+  const changePathHandler = (event) => {
+    setEndpointPath(event.target.value);
+  }
+
+  // Changes the data for each status object
+  const changeStatusHanlder = (event) => {
+    const row = event.target.dataset.row;
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormFields((prevState) => {
+      const newState = [...prevState];
+      if (name === 'status') {
+        newState[row].status = value;
+      }
+      else {
+        try {
+          JSON.parse(value);
+          setIsJsonValid(true);
+        } catch (err) {
+          setIsJsonValid(false);
+          console.log('Invalidr JSON structure. Check.');
+          // throw err;
+        }
+        newState[row].data = value;
+      }
+      return newState;
+    });
+  };
 
   const submitHanlder = async (event) => {
     event.preventDefault();
@@ -58,18 +107,30 @@ const NewPage = () => {
           formFields.map((row, index) => {
             return (
               <div style={{ margin: '50px' }} key={`row-${index}`}>
-                <select name="status" value={row.status} row={index} onChange={onChange}>
+                <select name="status" value={row.status} data-row={index} onChange={changeStatusHanlder}>
                   <option value="">select status</option>
                   {STATUS_LIST.map(status => <option value={status} key={status}>{status}</option>)}
                 </select>
                 <br />
                 <textarea
-                  onChange={onChange}
-                  name={`data-${index}`}
+                  onChange={changeStatusHanlder}
+                  data-row={index}
+                  name="data"
                   placeholder="Response Data"
-                  value={JSON.stringify(row.data)} />
+                  value={row.data}
+                  style={textAreaStyle} />
                 <br />
-                <button onClick={addNewStatus}>New Status</button>
+                <button
+                  onClick={addNewStatusGroup}
+                  disabled={oneGroupForEachStatus}>
+                  Add Status
+                </button>
+                <button
+                  data-row={index}
+                  onClick={removeStatusGroup}
+                  disabled={formFields.length === 1}>
+                  Remove Status
+                </button>
                 <hr />
               </div>
             );
